ed299c8f402d31286488ca550a1f483745c30e71
binikiya <biniyamkiya3682@gmail.com>
2025-05-13 19:55:23 +0300

    Join Quiery added

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -4,7 +4,7 @@
 SELECT * FROM Booking INNER JOIN User ON Booking.user_id = User.user_id;
 
 -- Left Join
-SELECT * FROM Property LEFT JOIN Review ON Property.property_id = Review.review_id;
+SELECT * FROM Property LEFT JOIN Review ON Property.property_id = Review.review_id ORDER BY property_id;
 
 -- Full Join
 SELECT * FROM User FULL OUTER JOIN Booking ON User.user_id = Booking.user_id;
