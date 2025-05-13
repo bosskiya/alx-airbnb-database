49deb6df1ede4277ad4dabd272e4d46d4a1d1b3c
binikiya <biniyamkiya3682@gmail.com>
2025-05-14 00:28:22 +0300

    Performance and Optimization Added

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -1,5 +1,25 @@
+-- Analyzing the query’s performance using EXPLAIN
+EXPLAIN 
+SELECT * FROM Booking b
+INNER JOIN User u ON b.user_id = u.user_id
+INNER JOIN Property p ON b.property_id = p.property_id
+LEFT JOIN Payment pay ON b.booking_id = pay.booking_id;
+
+EXPLAIN 
+SELECT * FROM Booking b
+INNER JOIN User u ON b.user_id = u.user_id
+INNER JOIN Property p ON b.property_id = p.property_id
+LEFT JOIN Payment pay ON b.booking_id = pay.booking_id
+WHERE b.status = 'confirmed' AND pay.payment_status = 'completed';
+
 -- Query that retrieves all bookings along with the user details, property details, and payment details
 SELECT * FROM Booking b
 INNER JOIN User u ON b.user_id = u.user_id
 INNER JOIN Property p ON b.property_id = p.property_id
 LEFT JOIN Payment pay ON b.booking_id = pay.booking_id;
+
+SELECT * FROM Booking b
+INNER JOIN User u ON b.user_id = u.user_id
+INNER JOIN Property p ON b.property_id = p.property_id
+LEFT JOIN Payment pay ON b.booking_id = pay.booking_id
+WHERE b.status = 'confirmed' AND pay.payment_status = 'completed';
